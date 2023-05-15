edf6d6e887482742aa2dca3d65bed9281ee3ecfa
Hekari <sszpytma6@gmail.com>
2023-05-15 17:17:20 +0200

    added search and view functionality

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
-from views import views
+from flask import Flask, render_template, request
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-app.register_blueprint(views,url_prefix="/")
+# Przykładowa lista zawodników
+zawodnicy = [
+    {'imie': 'Jan', 'nazwisko': 'Kowalski', 'pozycja': 'Napastnik'},
+    {'imie': 'Adam', 'nazwisko': 'Nowak', 'pozycja': 'Pomocnik'},
+    {'imie': 'Piotr', 'nazwisko': 'Nowicki', 'pozycja': 'Bramkarz'},
+    # Dodaj więcej zawodników według potrzeb
+]
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        szukany_tekst = request.form['tekst']
+        zawodnik = znajdz_zawodnika(szukany_tekst)
+        return render_template('wyniki.html', zawodnik=zawodnik)
+    return render_template('index.html')
+
+def znajdz_zawodnika(szukany_tekst):
+    for zawodnik in zawodnicy:
+        if szukany_tekst.lower() == f"{zawodnik['imie'].lower()} {zawodnik['nazwisko'].lower()}":
+            return zawodnik
+    return None
 
 if __name__ == '__main__':
-        app.run(debug=True,port=8000)
+    app.run(debug=True)
